d43eb9a3d76f0f10a621598531f13337f7c0ebe6
Ramón <ramonarj@ucm.es>
2019-10-26 21:00:11 +0200

    Muchos comentarios

--- Practica 2/Practica2.py
+++ Practica 2/Practica2.py
@@ -5,136 +5,151 @@ import scipy.integrate
 import scipy.optimize as opt
 from sklearn.preprocessing import PolynomialFeatures 
 
+## Carga el fichero csv especificado y lo devuelve en un array de numpy
 def carga_csv(file_name):
-    """carga el fichero csv especificado y lo devuelve en un array de numpy
-    """
     valores = read_csv(file_name, header=None).values
     
-    # suponemos que siempre trabajaremos con float
+    # Suponemos que siempre trabajaremos con float
     return valores.astype(float)
 
-def visualizacion_datos(X, Y, labelX, labelY, title=""): #Ejercicio 1.1
-    # Obtiene un vector con los índices de los ejemplos positivos
-    pos = np.where (Y == 1 )
+## Pinta los datos para una regresión logística, con '0' para las Y = 1 y '+' para las Y = 0 
+def visualizacion_datos(X, Y, labelX, labelY, title=""): 
 
-    # Dibuja los ejemplos con Y = 1
-    plt.scatter(X[pos, 0], X[pos, 1], marker='+' ,c='k', label="y = 1" )
+    # Obtienemos vectores con los índices de los ejemplos cuya Y es 0/1 (respectivamente)
+    pos0 = np.where (Y == 0 )
+    pos1 = np.where (Y == 1 )
 
-    # Obtiene un vector con los índices de los ejemplos negativos
-    pos = np.where (Y == 0 )
-
-    # Dibuja los ejemplos con Y = 0
-    plt.scatter(X[pos, 0], X[pos, 1], marker='o' ,c='darkkhaki', label="y = 0"  )
-
-    #HUD
+    # Los dibujamos con distintos símbolos
+    plt.scatter(X[pos0, 0], X[pos0, 1], marker='o' ,c='darkkhaki', label="y = 0"  )
+    plt.scatter(X[pos1, 0], X[pos1, 1], marker='+' ,c='k', label="y = 1" )   
+    
+    # Pintamos los ejes, el título y la leyenda
     plt.xlabel(labelX)
     plt.ylabel(labelY)
     plt.title(title)
     plt.legend()
     plt.legend(loc='upper right')
 
-
+## Pinta la frontera de decisión en la regresión lineal (como una recta)
 def pinta_frontera_recta(X, Y, theta):
+    # Mínimo y máximo valor para cada componente de la X
     x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
     x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
 
+    # Hacemos un meshgrid
     xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
     np.linspace(x2_min, x2_max))
 
+    # Hallamos el sigmoide y cambiamos su tamaño
     h = sigmoid(np.c_[np.ones((xx1.ravel().shape[0], 1)),
     xx1.ravel(),
     xx2.ravel()].dot(theta))
     h = h.reshape(xx1.shape)
 
-    # el cuarto parámetro es el valor de z cuya frontera se
-    # quiere pintar
+    # Pintamos la frontera para z = 0.5
     plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='b')
 
-    #plt.savefig("frontera.pdf")
-
-
+## Pinta la frontera de decisión en la regresión logística (más precisa que la recta)
 def plot_decisionboundary(X, Y, theta, poly):
+    #Igual que pinta_frontera_recta pero con el poly_fit_transform
     x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
     x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
+
     xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
     np.linspace(x2_min, x2_max))
+
     h = sigmoid(poly.fit_transform(np.c_[xx1.ravel(), xx2.ravel()]).dot(theta))
     h = h.reshape(xx1.shape)
+
     plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='g')
 
     
-
+## Calcula el sigmoide del número z
 def sigmoid(z): #Ej. 1.2
     s = 1 / (1 + np.exp(-z))
     return s
 
+## Calcula el coste sobre los ejemplos de entrenamiento para un cierto valor de theta
 def cost(theta, X, Y):
     H = sigmoid(np.matmul(X, theta))
     cost = (- 1 / (len(X))) * (np.dot(Y, np.log(H)) + np.dot((1 - Y), np.log(1 - H)))
     return cost
 
-##Estará bien??
+## Calcula el coste de forma regularizada para un cierto lambda
 def regularizedCost(theta, lamda, X, Y):
     regCost = cost(theta, X, Y)
-    regCost = regCost + (lamda / 2*(len(X))) * np.sum(theta**2)
+
+    #Añadimos el sumatorio de thetas al cuadrado al valor del coste normal
+    regCost = regCost + (lamda / 2*(len(X))) * np.sum(theta**2) 
     return regCost
 
+## Calcula el gradiente sobre los ejemplos de entrenamiento para un cierto valor de theta
 def gradiente(theta, X, Y):
     H = sigmoid(np.matmul(X, theta))
     grad = (1/ len(Y)) * np.matmul(X.T, H-Y)
     return grad
 
-##Estará bien??
+## Calcula el gradiente de forma regularizada para un cierto lambda
 def regularizedGradient(theta, lamda, X, Y):
     regGrad = gradiente(theta, X, Y)
     aux = np.copy(theta)
     aux[0] = 0
+    #Añadimos el sumatorio de thetas al valor del gradiente normal
     regGrad = regGrad + (lamda / len(Y) * aux)
     return regGrad
 
+## Calcula la predicción sobre x para un cierto theta
 def h(x, theta):
     return np.dot(x, theta[np.newaxis].T)
 
+## Calcula el porcentaje de ejemplos de entrenamiento que han sido clasificados correctamente
 def calcula_porcentaje(X, Y, theta):
 
+    #Calculamos el sigmoide
     n = Y.shape[0]
     H = sigmoid(np.matmul(X, theta))
 
+    # Ponemos los elementos de H a 1 (si hi > 0.5) o a 0 (si hi < 0.5)
     H = np.where (H > 0.5, H, 0)
     H = np.where (H < 0.5, H, 1)
 
+    # Vemos cuantos de ellos coinciden con Y y devolvemos el porcentaje sobre el total de ejemplos
     coinciden = np.where ( Y == H )
     aciertos = len(coinciden[0])
 
     return (aciertos / n) * 100
     
-
+## Ejercicio 1: regresión lineal
 def Ejercicio1():
-    #Leemos los valores de la matriz, nota examen 1, nota examen 2 y si ha sido aceptado
+    #Leemos los valores de la matriz de datos
     valores = carga_csv("ex2data1.csv")
     X = valores[:, :-1]
     Y = valores[:, -1]
 
+    #Inicializamos theta y ponemos la columna de 1's a las X
     theta = np.zeros((X.shape[1] + 1))
-
     m = X.shape[0]
     unos = np.ones((m, 1))
     unosX = np.hstack((unos, X))
 
-
+    # Calculamos el vector de pesos óptimo gracias a la función fmin_tnc (que recibe el valor inicial, 
+    # las funciones de coste y gradiente y los parámetros extra necesarios (X, Y y lambda))
     result = opt.fmin_tnc(func = cost, x0=theta, fprime=gradiente, args=(unosX,Y))
     theta_opt = result[0]
 
     #Porcentaje 
     porc = calcula_porcentaje(unosX, Y, theta_opt)
 
-    #Gráfica
+    # Pintamos los datos y la frontera de decisión  
     title = str(porc) + "% de ejemplos clasificados correctamente"
     visualizacion_datos(X, Y, "Exam 1 score", "Exam 2 score", title)
     pinta_frontera_recta(X, Y, theta_opt)
+
+    # Guardamos la gráfica y cerramos
     plt.savefig("Ejercicio1.pdf")
+    plt.close()
 
-
+## Ejercicio 2: regresión logística y regularización (recibe el valor de lambda)
 def Ejercicio2(lamda):
     #Leemos los valores de la matriz de datos
     valores = carga_csv("ex2data2.csv")
@@ -146,9 +161,7 @@ def Ejercicio2(lamda):
     polyX = poly.fit_transform(X)
     theta = np.zeros((polyX.shape[1]))
 
-
-    # Calculamos el vector de pesos óptimo gracias a la función fmin_tnc (que recibe el valor inicial, 
-    # las funciones de coste y gradiente y los parámetros extra necesarios (X, Y y lambda))
+    # Calculamos el vector de pesos óptimo igual que en el ejercicio 1 (ahora pasamos también lambda para la regularización)
     result = opt.fmin_tnc(func = regularizedCost, x0=theta, fprime=regularizedGradient, args=(lamda, polyX, Y))
     theta_opt = result[0]
 
@@ -164,4 +177,7 @@ def Ejercicio2(lamda):
     plt.savefig("Ejercicio2_lambda" + str(lamda) + ".pdf")
     plt.close()
 
+Ejercicio1()
+Ejercicio2(0.1)
+Ejercicio2(1)
 Ejercicio2(10)

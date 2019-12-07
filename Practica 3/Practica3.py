f5ffeaf021c8baa3ae9634928f5765887734a156
Ramón <ramonarj@ucm.es>
2019-12-07 13:36:06 +0100

    Creado archivo de utilidades
    Porque mola, porque es la moda

--- Practica 3/Practica3.py
+++ Practica 3/Practica3.py
@@ -6,68 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fmin_tnc
 from sklearn.preprocessing import PolynomialFeatures 
 from scipy.io import loadmat
-
-
-def h(x, theta):
-    '''
-    Calcula la predicción sobre x para un cierto theta
-    '''
-    return np.dot(x, theta[np.newaxis].T)
-
-    
-def hMatrix(x, theta):
-    '''
-    Calcula la predicción sobre x para una cierta matriz de thetas 
-    '''
-    return np.dot(x, theta.T)
-
-def sigmoid(z): 
-    '''
-    Calcula el sigmoide del número z
-    '''
-    s = 1 / (1 + np.exp(-z))
-    return s
-
-
-def regularizedCost(theta, lamda: float, X, Y):
-    '''
-    Calcula el coste para los ejemplos, pesos y término de regularización dados
-    '''
-    #Variables auxiliares
-    m = X.shape[0]
-    H = sigmoid(np.matmul(X, theta))
-
-    # Coste cuando Y = 1
-    costeUno = np.dot(Y, np.log(H))
-    # Coste cuando Y = 0
-    costeCero = np.dot((1 - Y), np.log(1 - H))
-    # Término de regularización (sumado al coste original)
-    regTerm = lamda / (2 * m) * np.sum(theta**2) 
-
-    return -1 / m * (costeUno + costeCero) + regTerm
-
-def regularizedGradient(theta, lamda: float, X, Y):
-    '''
-    Calcula el gradiente con los ejemplos, pesos y término de regularización dados
-    '''
-    #Variables auxiliares
-    m = X.shape[0]
-    H = sigmoid(np.matmul(X, theta))
-
-    # Cálculo del gradiente
-    grad = (1/ m) * np.matmul(X.T, H-Y)
-
-    # No queremos usar la primera componente de theta (nos la guardamos)
-    aux = theta[0]
-    theta[0] = 0
-
-    # Término de regularización
-    regTerm = lamda / m * theta
-
-    #Devolvemos su valor a theta
-    theta[0] = aux
-
-    return grad + regTerm
+from ML_utilities import h, hMatrix, sigmoid, regularizedCost, regularizedGradient
 
 def oneVsAll(X: np.array, y: np.array, num_etiquetas: int, reg: float):
     '''
@@ -138,12 +77,6 @@ def Ejercicio1(lamda):
     X = data['X'] # Cada ejemplo de entrenamiento en una fila (5000x400)
     y = data['y'] # 1 - 10 (el 10 es 0)
     num_etiquetas = 10
-
-    # Cogemos 10 muestras aleatorias y las pintamos
-    #sample = np.random.choice(X.shape[0], 10)
-    #plt.imshow(X[sample, :].reshape(-1, 20).T)
-    #plt.axis('off')
-    #plt.show()
 
     #Inicializamos theta y ponemos la columna de 1's a las X
     m = X.shape[0]

8b6020f9943562c88ee76d44219487265608ebab
Ramón <ramonarj@ucm.es>
2020-01-19 17:20:10 +0100

    Terminada primera parte del ejercicio 6
    Con muuuchas gráficas

--- Practica 6/Practica6.py
+++ Practica 6/Practica6.py
@@ -22,38 +22,143 @@ def muestraDatos(X, y, title):
     plt.title(title)
     plt.plot(X[:,0][pos], X[:,1][pos], "k+")
     plt.plot(X[:,0][neg], X[:,1][neg], "yo")
-    plt.show()
 
 
-
-# Create a funtion that plots a non-linear decision boundary.
-def pintaFrontera(X, y, model, title):
+def pintaFrontera(X, y, svm, title):
     """
-
+    Pinta una frontera de decisión (lineal / no lineal)
     """
-    # Get the separating hyperplane.
-    w = model.coef_[0]
-    a = -w[0] / w[1]
-    # Only 2 points are required to define a line, e.g. min and max.
-    xx = np.array([X[:,0].min(), X[:,0].max()])
-    yy = a * xx - (model.intercept_[0]) / w[1]
-    # Plot the separating line.
-    plt.plot(xx, yy, 'r-', linewidth=2)
-    # Plot the training data.
-    muestraDatos(X, y, title)
+    #Creamos el grid
+    x1 = np.linspace(X[:, 0].min(), X[:, 0].max(), 100)
+    x2 = np.linspace(X[:, 1].min(), X[:, 1].max(), 100)
+    x1, x2 = np.meshgrid(x1, x2)
+
+    #Presicción
+    yp = svm.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape)
+    pos = (y == 1)
+    neg = (y == 0)
+
+    # Pintamos
+    plt.figure()
+    #Datos
+    plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+',  label="y = 0")
+    plt.scatter(X[neg, 0], X[neg, 1], color='yellow',  
+    edgecolors='black', marker='o', label="y = 1")
+
+    #Titulo y leyenda
+    plt.title(title)
+    plt.legend()
+    #Frontera de decisión
+    plt.contour(x1, x2, yp)
+
+
+def calcula_porcentaje(Y, Y2, digitsNo: int):
+    '''
+    Calcula el porcentaje de aciertos 
+    '''
+
+    m = Y.shape[0]
+
+    # Vemos cuántos de ellos coinciden con Y 
+    coinciden = ( Y== Y2 )
+    aciertos = np.sum(coinciden)
+
+    # Porcentaje sobre el total de ejemplos redondeado
+    return round((aciertos / m) * 100, digitsNo)
 
 def Ejericio1(reg):
+    '''
+    Con kernel lineal
+    '''
     # 1. Cargamos los datos
     data = loadmat('ex6data1.mat') 
-    X = data['X'] # (5000x400)
-    y = data['y'].ravel() #(5000,)
+    X = data['X'] 
+    y = data['y'].ravel() 
 
-    # Hacemos el SVM con kernel lineal
+    # Hacemos el SVM con kernel lineal (el conjunto es linalmente separable)
     svm = SVC(kernel='linear', C=reg)
     svm.fit(X, y)
 
     # Pintamos la frontera
-    pintaFrontera(X, y, svm, "Kernel lineal")
+    pintaFrontera(X, y, svm, "Kernel lineal C=" + str(reg))
+
+    # Guardamos la gráfica y cerramos
+    #plt.show()
+    plt.savefig("SVMLineal_C=" + str(reg) + ".pdf")
+    plt.close()
+
+def Ejericio2(reg, sigma):
+    '''
+    Con kernel gaussiano
+    '''
+    # 1. Cargamos los datos
+    data = loadmat('ex6data2.mat') 
+    X = data['X'] 
+    y = data['y'].ravel()
+
+    # Hacemos el SVM con kernel gaussiano
+    svm = SVC(kernel='rbf', C=reg, gamma=1 / (2 * sigma ** 2))
+    svm.fit(X, y)
+
+    # Pintamos la frontera
+    pintaFrontera(X, y, svm, ("Kernel gaussiano C=" + str(reg) + ", σ=" + str(sigma)))
+
+    # Guardamos la gráfica y cerramos
+    #plt.show()
+    plt.savefig("SVMGaussiano_C=" + str(reg) + "_sigma" + str(sigma) + ".pdf")
+    plt.close()
+
+def Ejercicio3(values):
+    '''
+    Con kernel gaussiano y eligiendo bien los parámetros C y σ
+    '''
+    # 1. Cargamos los datos
+    data = loadmat('ex6data3.mat') 
+    X = data['X'] 
+    y = data['y'].ravel()
+    Xval = data['Xval'] 
+    Yval = data['yval']
+
+    #Lista de modelos
+    svm = []
+    
+    mejor = 0 #El mejor modelo con la validación
+    mejor_porc = 0
+
+    cOPt = values[0]
+    sigmaOpt = values[0]
+
+    #Todas las posibles combinaciones de modelos con los valores dados para C y sigma
+    actual = 0
+    for i in range (len(values)):
+        for j in range (len(values)):
+            # Hacemos el SVM con kernel gaussiano
+            svm.append(SVC(kernel='rbf', C=values[i], gamma=1 / (2 * values[j] ** 2)))
+            svm[actual].fit(X, y)
+
+            #Vemos el porcentaje de aciertos
+            h = svm[actual].predict(Xval)
+            porc = calcula_porcentaje(Yval.ravel(), h, 4)
+
+            #Si el porcentaje es mejor que el máximo, actualizamos 
+            if(porc > mejor_porc):
+                mejor = actual
+                mejor_porc = porc
+                cOpt = values[i]
+                sigmaOpt = values[j]
+            
+            actual+=1 #Avanzamos
+
+    print("Mejor porcentaje:" + str(mejor_porc) + "% con C=" + str(cOpt)+ ", σ=", str(sigmaOpt))
 
 
-Ejericio1(100)
+    # Pintamos la frontera
+    pintaFrontera(X, y, svm[mejor], "Kernel gaussiano óptimo, C=" + str(cOpt) + ", σ=" + str(sigmaOpt) + ". Precisión:" + str(mejor_porc) + "%")
+    plt.savefig("SVMGaussianoOptimo_C=" + str(cOpt) + "_sigma" + str(sigmaOpt) + ".pdf")
+    plt.close()
+
+
+values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+#Ejericio1(100)
+#Ejericio2(1, 0.1)
+Ejercicio3(values)
